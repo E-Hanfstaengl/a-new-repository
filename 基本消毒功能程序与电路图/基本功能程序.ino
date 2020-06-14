8d4889617e8f8a66704db95be043b70746a19c90
zhangkai <384816721@qq.com>
2020-06-14 14:07:42 +0800

    基本功能电路图与程序
    更改了一下

--- 基本消毒功能程序与电路图/基本功能程序.ino
+++ 基本消毒功能程序与电路图/基本功能程序.ino
@@ -1,6 +1,7 @@
 int purplePin = 2 ;
-int redPin = 4 ;
+int yellowPin = 4 ;
 int greenPin = 5 ;
+int redPin = 7 ; 
 int ledPin = 9 ;
 
 int speakPin = 6 ;
@@ -8,37 +9,51 @@ int speakPin = 6 ;
 int pressPin = 3;
 
 int judge = 1 ;
-
+int kg = 1;
+int timer = 0 ;
+int i = 0 ;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(purplePin, OUTPUT);
   pinMode(redPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(pressPin, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    
+
   if ( digitalRead(pressPin) == LOW){
     judge = 0;
-    digitalWrite(redPin, HIGH);
+    digitalWrite(yellowPin, HIGH);
     delay(30000);
   }
   
   if (judge == 0) {
     digitalWrite(purplePin, HIGH);
-    digitalWrite(redPin, HIGH);
+    digitalWrite(yellowPin, HIGH);
     delay(1000*60*30);
     digitalWrite(purplePin, LOW);
-    digitalWrite(redPin, LOW);
+    digitalWrite(yellowPin, LOW);
     digitalWrite(speakPin, HIGH);
     digitalWrite(greenPin, HIGH);
     delay(10000);
     digitalWrite(speakPin, LOW);
     digitalWrite(greenPin, LOW);
+    kg = 0;
   }
   
-
-}
+  if (kg == 0) {
+    for (timer = 1 ; timer <= 6 ; timer++){
+      for (i = 1; i <= 60 ; i++){
+       delay(1000*60);
+      }
+    }  
+   digitalWrite(redPin, HIGH);
+   digitalWrite(speakPin, HIGH);
+   delay(30000);
+   digitalWrite(redPin, LOW); 
+   digitalWrite(speakPin, LOW);
+ }
+}  
+  
